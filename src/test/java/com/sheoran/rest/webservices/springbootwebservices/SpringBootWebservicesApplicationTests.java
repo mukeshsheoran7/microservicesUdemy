121f2c0a312543337c06788a9a89b22115c66304
msheoran <msheoran@cisco.com>
2021-05-19 17:09:11 +0530

    initial commit

---
+++ src/test/java/com/sheoran/rest/webservices/springbootwebservices/SpringBootWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheoran.rest.webservices.springbootwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
